ce378d5e7a38e363ef3c68092f706e6df06de69f
Zhivko Shopov <zhivkoshopov@edud028.kent.ac.uk>
2023-09-11 15:53:29 +0100

    added functionality to update state to each square when we click

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
 import {useState} from 'react'
 import './styles.css'
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue('X')
-  }
-
-  return <button className="square" onClick={handleClick}>{value}</button>;
+function Square({value, onSquareClick}) {
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null))
+
+  function handleClick(i) {
+    //Create a copy of the initial array
+    const nextSquare = squares.slice();
+    nextSquare[i] = 'X';
+    setSquares(nextSquare);
+  }
   
   return (
     <div className="board">
       <div className="board-row">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}></Square>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}></Square>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}></Square>
       </div>
       <div className="board-row">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}></Square>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}></Square>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}></Square>
       </div>
       <div className="board-row">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}></Square>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}></Square>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}></Square>
       </div>
     </div>
   );
